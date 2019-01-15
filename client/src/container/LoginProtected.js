f86ea59b90b49c6bbf6a7a014f69c58857a319aa
john2796 <jbmiranda22796@gmail.com>
2019-01-14 19:24:04 -0800

    adding styling for login

---
+++ client/src/container/LoginProtected.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "axios";
+
+const LoginProtected = () => {
+  return (
+    <div>
+      <h1>routes</h1>
+    </div>
+  );
+};
+
+export default LoginProtected;
